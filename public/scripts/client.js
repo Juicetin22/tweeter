ec50f6a7e71597a2c93f0c55c8bea988b1b8b3af
Justin Huang <justin.2260@gmail.com>
2021-12-16 22:11:19 -0500

    cleaned code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -33,20 +33,20 @@ $(() => {
 
   //function for creating a tweet from the user info object; these new tweets will have classes that will allow them to be stylized as per our css stylesheets
   const createTweetElement = (tweet) => {
-    //start with creating an article tag
-    const $tweet = $("<article class='unique-tweet'></article>");
+          //start with creating an article tag
+          const $tweet = $("<article class='unique-tweet'></article>");
     //followed by the first child of the article tag
     const $tweetHeader = $("<header class='tweet-head'></header>");
     //name and handle are children of the header tag
-    const $tweetName = $(`<p class="user"><img src=${tweet.user.avatars}>&nbsp&nbsp${tweet.user.name}</p>`);
+      const $tweetName = $(`<p class="user"><img src=${tweet.user.avatars}>&nbsp&nbsp${tweet.user.name}</p>`);
     const $tweetHandle = $(`<p class="handle">${tweet.user.handle}</p>`);
-    //next is the middle of the article(child of article) 
-    //note that changed this node to use .text() instead of a template literal to prevent cross-site scripting since it uses user input data!
-    const $tweetBody = $(`<p class="tweet-middle">`).text(tweet.content.text);
+      //next is the middle of the article(child of article) 
+      //note that changed this node to use .text() instead of a template literal to prevent cross-site scripting since it uses user input data!
+      const $tweetBody = $(`<p class="tweet-middle">`).text(tweet.content.text);
     //followed by a footer tag(child of article)
     const $tweetFooter = $("<footer class='tweet-foot'></footer>");
-    //date and images are children of the footer tag
-    const $tweetDate = $(`<p>${timeago.format(tweet.created_at)}</p>`);
+      //date and images are children of the footer tag
+      const $tweetDate = $(`<p>${timeago.format(tweet.created_at)}</p>`);
     //create a div that contains the flag, retweet, and heart icons
     const $tweetImages = $("<div class='images'><i class='fas fa-flag'></i><i class='fas fa-retweet'></i><i class='fas fa-heart'></i></div>");
    
@@ -86,17 +86,18 @@ $(() => {
     event.preventDefault();
     //remove error message node each time submit button is clicked so the message doesn't chain
     $(".error").remove();
+
     //if the number of characters in the textbox is equal to 0, ie. textbox is empty, prevent submission and alert user
     if ($("#tweet-text").val().length === 0) {
       //add a new child to the new-tweet container, containing the error message for empty tweet
-      $(".new-tweet").append($(`<p class="error">‼️ <b>Cannot post an empty tweet!</b> ‼️</p>`));
+      $(".new-tweet").append($(`<p class="error">⚠️ <b>Cannot post an empty tweet!</b> ⚠️</p>`));
       const $error = $(".error");
       //stop the function and have the error message appear
       return $error.hide().slideDown(250);
     //if number of characters in the textbox is > 140, prevent submission of tweet and alert user
     } else if ($("#tweet-text").val().length > 140) {
       //add a new child to the new-tweet container, containing the error message for max characters limit
-      $(".new-tweet").append($(`<p class="error">‼️ <b>Exceeds the maximum allowed number of characters!</b> ‼️</p>`));
+      $(".new-tweet").append($(`<p class="error">⚠️ <b>Exceeds the maximum allowed number of characters!</b> ⚠️</p>`));
       const $error = $(".error");
       //similar to above return; have error message appear
       return $error.hide().slideDown(250);
@@ -107,14 +108,14 @@ $(() => {
     const serialized = $(this).serialize();
     console.log(serialized);
 
-    //AJAX post request to where the tweets are stored
+    //shorthand AJAX post request to where the tweets are stored
     $.post("/tweets", serialized, (response) => {
       console.log(response);
       //want to clear the textbox after posting
       $("#tweet-text").val("");
       //want to set counter back to max limit after posting
       $(".counter").text(140);
-      //want to load the webpage again and see that the submitted tweet gets posted (without refreshing the page)  
+      //want to load the tweets on the webpage again and see that the submitted tweet gets posted (without refreshing the page)  
       loadTweets();
       console.log( $(".counter").text());
     });
