4a2dd3d6151ad9f9fa59d75e26b8229d3b4e8649
Justin Huang <justin.2260@gmail.com>
2021-12-16 23:10:18 -0500

    added comments to explain code; cleaned code; got rid of sample data to test function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,56 +1,21 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
+//shorthand for document.ready; want the script to run after the page is loaded
 $(() => {
-  //sample data to use for testing
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
 
   //function for creating a tweet from the user info object; these new tweets will have classes that will allow them to be stylized as per our css stylesheets
   const createTweetElement = (tweet) => {
-          //start with creating an article tag
-          const $tweet = $("<article class='unique-tweet'></article>");
-    //followed by the first child of the article tag
+    //start with creating an article tag and then gradually include each child and following subnodes, making sure to add in information from the user object data
+    const $tweet = $("<article class='unique-tweet'></article>");
     const $tweetHeader = $("<header class='tweet-head'></header>");
-    //name and handle are children of the header tag
-      const $tweetName = $(`<p class="user"><img src=${tweet.user.avatars}>&nbsp&nbsp${tweet.user.name}</p>`);
+    const $tweetName = $(`<p class="user"><img src=${tweet.user.avatars}>&nbsp&nbsp${tweet.user.name}</p>`);
     const $tweetHandle = $(`<p class="handle">${tweet.user.handle}</p>`);
-      //next is the middle of the article(child of article) 
-      //note that changed this node to use .text() instead of a template literal to prevent cross-site scripting since it uses user input data!
-      const $tweetBody = $(`<p class="tweet-middle">`).text(tweet.content.text);
-    //followed by a footer tag(child of article)
+    //note that we changed this node to use .text() instead of a template literal to prevent cross-site scripting since it uses user input data from form!
+    const $tweetBody = $(`<p class="tweet-middle">`).text(tweet.content.text);
     const $tweetFooter = $("<footer class='tweet-foot'></footer>");
-      //date and images are children of the footer tag
-      const $tweetDate = $(`<p>${timeago.format(tweet.created_at)}</p>`);
-    //create a div that contains the flag, retweet, and heart icons
+    const $tweetDate = $(`<p>${timeago.format(tweet.created_at)}</p>`);
+    //create a div that contains the flag, retweet, and like icons
     const $tweetImages = $("<div class='images'><i class='fas fa-flag'></i><i class='fas fa-retweet'></i><i class='fas fa-heart'></i></div>");
    
-    //we want to then combine them to make the desired tree structure
+    //we want to then combine them correctly to make the desired tree structure
     $tweetHeader.append($tweetName, $tweetHandle);
     $tweetFooter.append($tweetDate, $tweetImages);
     //this combines all of substructures to the article tag, which is the container for each individual tweet
@@ -59,10 +24,6 @@ $(() => {
     //we want to return this combined tweet
     return $tweet;
   };
-
-  // const $tweet = createTweetElement(tweetData);
-  // console.log($tweet);
-  // $("#tweets-container").append($tweet);
 
   //want to create a function that takes an array of tweets (in object form) and adds it to our main tweet container
   const renderTweets = (tweets) => {
@@ -78,13 +39,11 @@ $(() => {
     }
   };
 
-  renderTweets(data);
-
   //listen for a submit event
   $("#tweet-submit").on("submit", function(event) {
     //prevent submit to go to another page, as it normally does
     event.preventDefault();
-    //remove error message node each time submit button is clicked so the message doesn't chain
+    //remove error message node each time submit button is clicked so the errors don't chain after each submit
     $(".error").remove();
 
     //if the number of characters in the textbox is equal to 0, ie. textbox is empty, prevent submission and alert user
@@ -94,7 +53,8 @@ $(() => {
       const $error = $(".error");
       //stop the function and have the error message appear
       return $error.hide().slideDown(250);
-    //if number of characters in the textbox is > 140, prevent submission of tweet and alert user
+    
+      //if number of characters in the textbox is > 140, prevent submission of tweet and alert user
     } else if ($("#tweet-text").val().length > 140) {
       //add a new child to the new-tweet container, containing the error message for max characters limit
       $(".new-tweet").append($(`<p class="error">⚠️ <b>Exceeds the maximum allowed number of characters!</b> ⚠️</p>`));
@@ -103,27 +63,23 @@ $(() => {
       return $error.hide().slideDown(250);
     }
     
-    console.log("The Tweet was submitted!");
     //converts the form data into a query string as the server is configured to take query strings
     const serialized = $(this).serialize();
-    console.log(serialized);
 
     //shorthand AJAX post request to where the tweets are stored
     $.post("/tweets", serialized, (response) => {
-      console.log(response);
       //want to clear the textbox after posting
       $("#tweet-text").val("");
       //want to set counter back to max limit after posting
       $(".counter").text(140);
       //want to load the tweets on the webpage again and see that the submitted tweet gets posted (without refreshing the page)  
       loadTweets();
-      console.log( $(".counter").text());
     });
   });
 
   //function to load the tweets onto the web browser
   const loadTweets = () => {
-    //AJAX get request to the server tweet "database", handling a json response
+    //AJAX get request to the server, handling a json response
     $.ajax({
       url: "/tweets",
       method: "GET",
@@ -139,7 +95,6 @@ $(() => {
       } 
     });
   };
-
   //call the function to load up the tweets on the webpage
   loadTweets();
 
